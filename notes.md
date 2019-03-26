19753dd8900319a6817d924fd6f3f0a8704f2e01
Joshua Wood <JerichoWall@apu.edu>
2019-03-26 12:25:27 -0700

    learning

---
+++ notes.md
@@ -0,0 +1,12 @@
+roles table
+
+- id, integer, pk, not null, auto-increment
+- name, varchar(255), unique, not null
+
+
+
+API <> Query Builder <> Driver <> DB
+
+A Query Builder translates from JS to SQL
+
+Knex.js
--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,18 +1,50 @@
 const router = require('express').Router();
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/roles.db3'
+  },
+};
+
+const db = knex(knexConfig);
+
 
 router.get('/', (req, res) => {
-  // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  // returns a promise that resolves to all records in the table
+  db('roles')
+  .then(roles => {
+    res.status(200).json(roles);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  const { id } = req.params;
+
+  db('roles')
+  .where({ id })
+  .first()
+  .then(role => {
+    res.status(200).json(role);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 router.post('/', (req, res) => {
-  // add a role to the database
-  res.send('Write code to add a role');
+  // get back an array with the last id generated: [3]
+  db('roles')
+  .insert(req.body)
+  .then(is => {
+    
+  })
 });
 
 router.put('/:id', (req, res) => {
